caa10be89603f8ca8d0f5feebb8f6b167701c999
Clara <clarayhkim96@gmail.com>
2020-08-11 13:51:53 -0700

    clandie boilerplate

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build/')));
+
+// serve html
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
+});
+// catch all
+app.use('/', (req, res) => {
+  res.status(404).send('Page Not Found');
+});
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to Port ${PORT}...`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    publicPath: '/build',
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/env'],
+        },
+      },
+      {
+        test: /\.s?css/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
