b8df0d5f11e3d7621f09c0b109724f5b6fdd1d71
andieritter <58995450+andieritter@users.noreply.github.com>
2020-08-11 19:34:32 -0700

    re-adding index.tsx, strange merge delete

--- client/index.tsx
+++ client/index.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import store from './store';
+import * as React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store";
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
