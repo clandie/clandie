1cf6c0fb81647a8cf003fe5922883f7a8dfda2a8
andieritter <58995450+andieritter@users.noreply.github.com>
2020-08-14 19:46:52 -0700

    server refactored to typescript

--- server/server.ts
+++ server/server.ts
@@ -2,17 +2,18 @@
 erver domain to your server. Otherwise you may run into cross-origin resource s
 haring errors for your GraphQL server.
 */
-const cors = require('cors');
-const express = require('express');
-const path = require('path');
-const { ApolloServer } = require('apollo-server-express');
-const app = express();
-const PORT = 3000;
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import { ApolloServer } from 'apollo-server-express';
 require('dotenv').config();
+const cors = require('cors');
+const PORT = 3000;
 
-const schema = require('./schema');
-const resolvers = require('./resolvers');
-const db = require('./models/dbModel');
+const app: express.Application = express();
+
+import schema from './schema';
+import resolvers from './resolvers';
+import db from './models/dbModel';
 
 app.use(cors());
 app.use(express.json());
@@ -28,26 +29,28 @@ server.applyMiddleware({ app, path: '/graphql' });
 
 // serve html
 // multiple endpoints in array as first arg to get method for serving index.html
-app.get(['/', '/home', '/signup'], (req, res) => {
+app.get(['/', '/home', '/signup'], (req: Request, res: Response) => {
   res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
 // catch all
-app.use('*', (req, res) => {
+app.use('*', (req: Request, res: Response) => {
   res.status(404).send('Page Not Found');
 });
 
 // global error handler
-app.use((err, req, res, next) => {
-  const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 400,
-    message: { err: 'error occurred' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
-});
+app.use(
+  (err: Error, req: Request, res: Response, next: NextFunction): Response => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Apollo Server listening on ${PORT}/graphql...`);
