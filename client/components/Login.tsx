200b8992796b0ffd4dc1e350ece3f45eda429737
Clara <clarayhkim96@gmail.com>
2020-08-17 14:23:17 -0700

    JSX single to double quotes

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -5,25 +5,25 @@ import { Link } from 'react-router-dom';
 
 const Login: React.FC = () => {
   return (
-    <div className='login-container'>
-      <div className='login-img'></div>
-      <div className='login-box'>
-        <Form className='login-form'>
-          <Form.Group controlId='formBasicEmail'>
+    <div className="login-container">
+      <div className="login-img"></div>
+      <div className="login-box">
+        <Form className="login-form">
+          <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type='email' placeholder='Enter email' />
+            <Form.Control type="email" placeholder="Enter email" />
           </Form.Group>
-          <Form.Group controlId='formBasicPassword'>
+          <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Password' />
+            <Form.Control type="password" placeholder="Password" />
           </Form.Group>
-          <div className='login-btn'>
-            <Button variant='primary' type='submit' block>
+          <div className="login-btn">
+            <Button variant="primary" type="submit" block>
               Login
             </Button>
           </div>
         </Form>
-        <Link to='/signup'>Don't have an account? Sign up here!</Link>
+        <Link to="/signup">Don't have an account? Sign up here!</Link>
       </div>
     </div>
   );
--- client/index.tsx
+++ client/index.tsx
@@ -1,13 +1,13 @@
-import * as React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import App from "./App";
-import store from "./store";
-import "./stylesheets/styles.scss";
+import * as React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './store';
+import './stylesheets/styles.scss';
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
