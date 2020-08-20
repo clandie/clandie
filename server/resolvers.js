4d6e7fd1e8087b92a7414d2d292841fe547f2da8
andieritter <58995450+andieritter@users.noreply.github.com>
2020-08-20 14:40:54 -0700

    added createBoard to schema.js and resolvers.js - can now add a new board for a specific user

--- server/resolvers.js
+++ server/resolvers.js
@@ -37,6 +37,21 @@ module.exports = {
       const deletedUser = await postgresDB.query(text, params);
       return deletedUser.rows[0];
     },
+
+    // updateUser: async (parent, {name, email, password}, {postgresDB}) => {
+    //   const text = '';
+    //   const params = [];
+    //   const updatedUser = postgresDB.query(text, params);
+    //   return updatedUser.rows[0];
+    // },
+
+    createBoard: async (parent, { name, _id }, { postgresDB }) => {
+      const text =
+        'INSERT INTO boards (name, users_id) VALUES ($1, $2) RETURNING *';
+      const params = [name, _id];
+      const newBoard = await postgresDB.query(text, params);
+      return newBoard.rows[0];
+    },
   },
 
   User: {
