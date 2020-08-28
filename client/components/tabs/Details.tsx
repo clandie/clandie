96e6a9e6f22a6caacb006d24908f730da5995698
Clara <clarayhkim96@gmail.com>
2020-08-28 16:39:59 -0700

    added functions to details tab component to handle change and save logic

--- client/components/tabs/Details.tsx
+++ client/components/tabs/Details.tsx
@@ -1,7 +1,42 @@
 import React, { Component } from 'react';
 import { Form, Col, Button } from 'react-bootstrap';
 
-class Details extends Component {
+interface IDetailProps {}
+interface IDetailState {
+  location?: string | null;
+  salary?: string | null;
+  url?: string | null;
+}
+class Details extends Component<IDetailProps, IDetailState> {
+  constructor(props: IDetailProps) {
+    super(props);
+
+    this.state = {
+      location: null,
+      salary: null,
+      url: null,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+  }
+
+  handleChange(e: any) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSave(e: any) {
+    e.preventDefault();
+    const detailsObj: IDetailState = {
+      location: this.state.location,
+      salary: this.state.salary,
+      url: this.state.url,
+    };
+
+    console.log('details', detailsObj);
+  }
+
   render() {
     return (
       <div className="detailsTab">
@@ -9,19 +44,36 @@ class Details extends Component {
           <Form.Row>
             <Form.Group as={Col} controlId="formLocation">
               <Form.Label>Location</Form.Label>
-              <Form.Control type="text" placeholder="Location" />
+              <Form.Control
+                type="text"
+                name="location"
+                placeholder="Location"
+                onChange={this.handleChange}
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formSalary">
               <Form.Label>Salary</Form.Label>
-              <Form.Control type="text" placeholder="Salary" />
+              <Form.Control
+                type="text"
+                name="salary"
+                placeholder="Salary"
+                onChange={this.handleChange}
+              />
             </Form.Group>
           </Form.Row>
           <Form.Group controlId="formUrl">
             <Form.Label>URL</Form.Label>
-            <Form.Control type="text" placeholder="URL" />
+            <Form.Control
+              type="text"
+              name="url"
+              placeholder="URL"
+              onChange={this.handleChange}
+            />
           </Form.Group>
-          <Button type="submit">Save</Button>
+          <Button type="submit" onClick={this.handleSave}>
+            Save
+          </Button>
         </Form>
       </div>
     );
