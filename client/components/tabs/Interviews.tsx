e1d07ba9c10c841eae081dbfe8ab03ddfcced411
andieritter <58995450+andieritter@users.noreply.github.com>
2020-09-05 23:46:30 -0700

    setting up interviews tab on job modal

--- client/components/tabs/Interviews.tsx
+++ client/components/tabs/Interviews.tsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import { Form /*Col, Button*/ } from 'react-bootstrap';
+import { IInterviews } from '../../constants/types';
 
 class Interviews extends Component {
+  constructor(props: IInterviews) {
+    super(props);
+  }
+
   render() {
-    return <div className="interviewsTab"></div>;
+    return (
+      <div className="interviewsTab">
+        <Form>
+          <Form className="row">
+            <Form className="label">Title</Form>
+          </Form>
+          <Form className="row">
+            <Form className="label">Date</Form>
+            <Form className="label">Time</Form>
+          </Form>
+          <Form className="row">Notes</Form>
+        </Form>
+      </div>
+    );
   }
 }
 export default Interviews;
