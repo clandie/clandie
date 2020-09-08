4df0734fe2d9efe76e57c715355037b61ef9d60b
andieritter <58995450+andieritter@users.noreply.github.com>
2020-09-08 10:31:50 -0700

    updated error handling for userResolvers

--- server/resolvers/userResolvers.js
+++ server/resolvers/userResolvers.js
@@ -2,22 +2,15 @@ const { AuthenticationError } = require('apollo-server');
 
 module.exports = {
   Query: {
-    // gets all users
-    users: async (parent, args, { postgresDB }) => {
-      const text = `SELECT * FROM users`;
-      const users = await postgresDB.query(text);
-      return users.rows;
-    },
-    // gets a single user
     user: async (parent, { email, password }, { postgresDB }) => {
       try {
         const text = `SELECT * FROM users WHERE email=$1 AND password=$2`;
         const params = [email, password];
         const user = await postgresDB.query(text, params);
-        // may have to change the error handling
         if (!user.rows[0]) throw new AuthenticationError();
         return user.rows[0];
       } catch (err) {
+        console.log('An error occurred in user resolver: ', err);
         if (err.extensions.code === 'UNAUTHENTICATED') {
           err.extensions.message =
             'User is not authenticated. Please log in or create an account.';
@@ -37,44 +30,52 @@ module.exports = {
 
   User: {
     boards: async (parent, args, { postgresDB }) => {
-      const id = parent._id;
-      const text = 'SELECT * FROM boards WHERE users_id=$1';
-      const params = [id];
-      const boards = await postgresDB.query(text, params);
-      return boards.rows;
+      try {
+        const id = parent._id;
+        const text = 'SELECT * FROM boards WHERE users_id=$1';
+        const params = [id];
+        const boards = await postgresDB.query(text, params);
+        return boards.rows;
+      } catch (err) {
+        console.log('An error occurred in User.boards resolver: ', err);
+        return err;
+      }
     },
   },
 
   Mutation: {
     createUser: async (parent, { name, email, password }, { postgresDB }) => {
-      const text = `
+      try {
+        const text = `
         INSERT INTO 
         users (name, email, password) 
         VALUES ($1, $2, $3) 
         RETURNING *
       `;
-      const params = [name, email, password];
-      const newUser = await postgresDB.query(text, params);
-      return newUser.rows[0];
+        const params = [name, email, password];
+        const newUser = await postgresDB.query(text, params);
+        return newUser.rows[0];
+      } catch (err) {
+        console.log('An error occurred in createUser resolver: ', err);
+        return err;
+      }
     },
 
     deleteUser: async (parent, { email }, { postgresDB }) => {
-      const text = `
+      try {
+        const text = `
         DELETE FROM 
         users 
         WHERE email=$1 
         RETURNING *
       `;
-      const params = [email];
-      const deletedUser = await postgresDB.query(text, params);
-      return deletedUser.rows[0];
+        const params = [email];
+        const deletedUser = await postgresDB.query(text, params);
+        return deletedUser.rows[0];
+      } catch (err) {
+        console.log('An error occurred in deleteUser resolver: ', err);
+        return err;
+      }
     },
-
-    // updateUser: async (parent, {name, email, password}, {postgresDB}) => {
-    //   const text = '';
-    //   const params = [];
-    //   const updatedUser = postgresDB.query(text, params);
-    //   return updatedUser.rows[0];
-    // },
   },
 };
